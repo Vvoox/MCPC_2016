f39ce90760bc00575eb793f446a89d8fe3e94f30
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2019-04-21 13:30:43 +0100

    Add files via upload

--- Encrypted_mystery/src/com/example/java/Main.java
+++ Encrypted_mystery/src/com/example/java/Main.java
@@ -4,81 +4,103 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int T=0,N=0;
+    public static int T = 0, N = 0;
 
     public static void main(String[] args) {
-    System.out.println("Hello we need to help to know if the strings is Anti-sym or not : ");
-    System.out.println("Insert your input :");
-    Scanner cs = new Scanner(System.in);
-    Scanner nm = new Scanner(System.in);
-    Scanner stg = new Scanner(System.in);
-    T = cs.nextInt();
+        System.out.println("Hello we need to help to know if the strings is Anti-sym or not : ");
+        System.out.println("Insert your input :");
+        Scanner cs = new Scanner(System.in);
+        Scanner nm = new Scanner(System.in);
+        Scanner stg = new Scanner(System.in);
+        T = cs.nextInt();
+        String number = " ";
+        for (int i = 0; i < T; i++) {
 
-    String number = " ";
-    for(int i = 0  ; i <T ; i++){
+            N = nm.nextInt();
+            number = stg.nextLine();
+            int table1[] = new int[N];
+            String table[] = number.split("", 0);
+            for (String a : table) {
 
-        N=nm.nextInt();
-        number = stg.nextLine();
-        int table1[]=new int[N];
-        String table[] = number.split("",0);
-        for(String a : table){
+                System.out.print(a + " ");
 
-            System.out.print(a+" ");
+            }
+            for (int j = 0; j < N; j++) {
 
+                table1[j] = Integer.parseInt(table[j]);
+            }
+            System.out.println();
+            reverse(table1);
 
         }
-        for(int j=0 ; j<N ; j++ ){
-
-            table1[j]=Integer.parseInt(table[j]);
-        }
-        System.out.println();
-        reverse(table1);
-
     }
-    }
-    public static void reverse(int[] tab){
+
+    public static void reverse(int[] tab) {
 
         int reverse[] = new int[N];
-        int j=N-1;
-        for(int i=0 ; i<N ; i++){
+        int j = N - 1;
+        for (int i = 0; i < N; i++) {
 
-            reverse[i]=tab[j];
+            reverse[i] = tab[j];
             j--;
 
         }
-        for(int i=0 ; i<N ; i++){
+        for (int i = 0; i < N; i++) {
 
-            System.out.print(reverse[i]+" ");
+            System.out.print(reverse[i] + " ");
         }
         System.out.println();
-        flip(reverse);
+        flip(reverse, tab);
 
     }
-    public static void flip(int tab[]){
 
-        int flip[]=new int[N];
-        int l=0;
-        for(int i=0 ; i<N ; i++){
+    public static void flip(int tab[], int tab1[]) {
 
-            if(tab[i]==1){
+        int flip[] = new int[N];
+        int l = 0;
+        for (int i = 0; i < N; i++) {
 
-                flip[l]=0;
+            if (tab[i] == 1) {
+
+                flip[l] = 0;
                 l++;
             }
-            if(tab[i]==0){
+            if (tab[i] == 0) {
 
-                flip[l]=1;
+                flip[l] = 1;
                 l++;
             }
         }
 
+        for (int j = 0; j < N; j++) {
 
-
-        for(int j=0 ; j<N ; j++){
-
-            System.out.print(flip[j]+" ");
+            System.out.print(flip[j] + " ");
         }
         System.out.println();
+        antys(tab1, flip);
 
     }
-}
+
+    public static void antys(int[] tab1, int[] tab2) {
+
+        int index[] = new int[N];
+        for (int i = 0; i < N; i++) {
+
+            if (tab1[i] == tab2[i]) {
+
+                index[i] = i;
+                //l++;
+
+            }
+            else
+                index[i]=0;
+
+        }
+
+
+        System.out.println(index[N/2]);
+
+        }
+
+    }
+
